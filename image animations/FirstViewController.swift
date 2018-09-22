e30a09780828ea58192cd8476541b2d23af8b42b
K.K <koceto1973@yahoo.ca>
2018-09-22 14:51:36 +0300

    manual animation ok

--- image animations/FirstViewController.swift
+++ image animations/FirstViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        imageView.image = UIImage(named: "frame_2_delay-0.07s.png")
     }
 
-
+    var counter:Int = 2
+    @IBAction func Next(_ sender: Any) {
+        if counter < 45 {
+            counter += 1
+            imageView.image = UIImage(named: "frame_\(counter)_delay-0.07s.png")
+        } else {
+            counter = 2
+            imageView.image = UIImage(named: "frame_\(counter)_delay-0.07s.png")
+        }
+        
+    }
+    
 }
 
