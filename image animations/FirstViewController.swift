3b4cf72fc142f55163124b0e307c04a28704adc9
K.K <koceto1973@yahoo.ca>
2018-09-22 10:17:02 +0300

    blank tabbed app

---
+++ image animations/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  image animations
+//
+//  Created by К.К. on 22.09.18.
+//  Copyright © 2018 TeamK. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ image animations/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  image animations
+//
+//  Created by К.К. on 22.09.18.
+//  Copyright © 2018 TeamK. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
