f9b970f53b1282cc5405ef71cd7f80f7b3b86bee
K.K <koceto1973@yahoo.ca>
2018-09-22 18:26:47 +0300

    fadeIn slideIn grow effects

--- image animations/SecondViewController.swift
+++ image animations/SecondViewController.swift
@@ -38,7 +38,38 @@ class SecondViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         imageView.image = UIImage(named: "frame_2_delay-0.07s.png")
     }
-
+    @IBAction func FadeIn(_ sender: Any) {
+        imageView.alpha = 0
+        UIView.animate(withDuration: 1, animations: {
+            self.imageView.alpha = 1
+        })
+    }
+    @IBAction func SlideIn(_ sender: Any) {
+        imageView.center = CGPoint(x: imageView.center.x - 500, y: imageView.center.y )
+        UIView.animate(withDuration: 2, animations: {
+            self.imageView.center = CGPoint(x: self.imageView.center.x + 500, y: self.imageView.center.y )
+        })
+    }
+    @IBAction func Grow(_ sender: Any) {
+        let width  = imageView.frame.size.width
+        let height = imageView.frame.size.height
+        
+        imageView.frame = CGRect(x: imageView.frame.origin.x, y: imageView.frame.origin.y, width: 0, height: 0)
+        UIView.animate(withDuration: 2, animations: {
+            self.imageView.frame = CGRect(x: self.imageView.frame.origin.x, y: self.imageView.frame.origin.y, width: width, height: height)
+        })    }
+    
+    // stack the image set for the animation into array
+    // set folder "Relative to Group"
+    //for index in 2...45{
+    //var prefixName = "frame_"
+    //let suffuxName = "_delay-0.07s.png"
+    //prefixName += String(index)+suffuxName
+    //imageArray.append( UIImage(named: prefixName)!)
+    //}
+    // animate
+    //self.ImageView.animationImages = imageArray
+    //self.ImageView.animationDuration = 4
 
 }
 
